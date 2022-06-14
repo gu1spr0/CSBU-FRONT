15ad13dced02f0794f9c710447a8cfb00fd24167
Israel Guillermo <learsi.guil2010@gmail.com>
2022-06-14 18:58:00 -0400

    CORS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //URL_API: 'http://178.62.200.136:4701/'
-  URL_API: 'http://localhost:4701/'
+  URL_API: 'http://178.62.200.136:4701/'
+  //URL_API: 'http://localhost:4701/'
 };
 
 /*
